e75697ec01cd45f103c319ca65743f8f9a753864
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2023-02-13 23:00:27 +0800

    28x28 (RGB) only

--- convo.py
+++ convo.py
@@ -2,7 +2,7 @@ from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, BatchNor
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 
-# Read the dataset "pythonProject\dataset" and create a image generator with augmentation
+# Image generator with augmentation
 train_datagen = ImageDataGenerator(rescale=1. / 255, rotation_range=40, width_shift_range=0.2, height_shift_range=0.2,
                                    shear_range=0.2, zoom_range=0.2, horizontal_flip=True, fill_mode='nearest')
 
