eb07af9a541cd58df9189ac0e379b8cdd94a8620
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2023-02-13 22:19:07 +0800

    sheeshhh

--- convo.py
+++ convo.py
@@ -1,4 +1,4 @@
-from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, BatchNormalization, MaxPool2D
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 
@@ -6,27 +6,26 @@ from keras.models import Sequential
 train_datagen = ImageDataGenerator(rescale=1. / 255, rotation_range=40, width_shift_range=0.2, height_shift_range=0.2,
                                    shear_range=0.2, zoom_range=0.2, horizontal_flip=True, fill_mode='nearest')
 
-training_set = train_datagen.flow_from_directory('dataset', target_size=(100, 100), batch_size=64, class_mode='binary')
+training_set = train_datagen.flow_from_directory('dataset', target_size=(28, 28), batch_size=64, class_mode='binary')
 
 model = Sequential([
-    Conv2D(32, (3, 3), activation='relu', input_shape=(100, 100, 3)),
-    MaxPooling2D(2, 2),
+    Conv2D(128, kernel_size=(5, 5), strides=1, padding='same', activation='relu', input_shape=(28, 28, 3)),
+    MaxPool2D(pool_size=(3, 3), strides=2, padding='same'),
+    Conv2D(64, kernel_size=(2, 2), strides=1, activation='relu', padding='same'),
+    MaxPool2D((2, 2), 2, padding='same'),
+    Conv2D(32, kernel_size=(2, 2), strides=1, activation='relu', padding='same'),
+    MaxPool2D((2, 2), 2, padding='same'),
 
-    Conv2D(64, (3, 3), activation='relu'),
-    MaxPooling2D(2, 2),
-
-    Conv2D(128, (3, 3), activation='relu'),
-    MaxPooling2D(2, 2),
-
-    Dropout(0.5),
     Flatten(),
-    Dense(64, activation='relu'),
-    Dense(29)
+    Dense(units=512, activation='relu'),
+    Dropout(rate=0.25),
+    Dense(29, activation='softmax')
 ])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
 model.summary()
-model.fit(training_set, epochs=10)
+model.fit(training_set, epochs=35)
 
 # Save the model
 model.save('model.h5')
